dd0aeb9b2de2a3fe7215f5313e08ca5df1a46e02
Dana Dzik <50225082+borgesius@users.noreply.github.com>
2024-01-02 16:15:44 -0800

    very basic test of RHC

---
+++ tests/videoIdent.js
@@ -0,0 +1,56 @@
+///////////////////////////////////////////////////////////////////////////
+// Copyright 2020 Roku, Inc.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//////////////////////////////////////////////////////////////////////////
+
+const rokuLibrary = require("../library/rokuLibrary");
+const expect = require("chai").expect;
+
+let library;
+
+describe("test_basic", () => {
+  before(async function () {
+    this.timeout(50000);
+    library = new rokuLibrary.Library("10.103.126.222");
+    await library.sideLoad("./channels/roku-deploy.zip", "rokudev", "password");
+  });
+
+  it("launchies", async function () {
+    this.timeout(15000);
+    await library.verifyIsChannelLoaded("dev");
+  });
+
+  it("loads VideoIdent element", async function () {
+    this.timeout(50000);
+    let res = await library.verifyIsScreenLoaded(
+      { elementData: [{ using: "tag", value: "VolleyVideoIdent" }] },
+      2,
+      2
+    );
+    expect(res).to.equal(true);
+  });
+
+  it("loads BideoIdent element", async function () {
+    this.timeout(10000);
+    let res = await library.verifyIsScreenLoaded(
+      { elementData: [{ using: "tag", value: "VolleyBideoIdent" }] },
+      2,
+      2
+    );
+    expect(res).to.equal(true);
+  });
+
+  after(async () => {
+    await library.close();
+  });
+});
